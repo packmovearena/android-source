afc42e306c28f500293366e91ffc9d7582d5fd55
Allabakash 697 <allabakash697@gamil.com>
2018-02-03 23:34:59 +0530

    Stable Google map version and related changes

--- src/pages/mapview/map/map.ts
+++ src/pages/mapview/map/map.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component , NgZone } from '@angular/core';
 import { NavController , ViewController , LoadingController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-import { Observable } from "rxjs/Observable";
 import { GoogleMap , GoogleMapsEvent} from "@ionic-native/google-maps";
+import {Observable} from "rxjs/Observable";
+
+declare var google: any;
 
 @Component({
   selector: 'page-map',
@@ -12,11 +14,14 @@ export class Map {
 
   map: GoogleMap;
   timer: any;
-  location: any;
   loader: any;
+  location: any;
+  pickupLocation: any;
+  currentLocation: any;
+  geoTracker: any;
 
   constructor(public navCtrl: NavController, public geolocation: Geolocation,
-              public viewCtrl: ViewController, public loadCtrl: LoadingController) {
+              public viewCtrl: ViewController, public loadCtrl: LoadingController, public ngZone : NgZone) {
 
   }
 
@@ -24,80 +29,100 @@ export class Map {
     this.loader = this.loadCtrl.create({
       content: 'Locating...'
     });
-    this.getCurrentLocation();
-    this.loadMap();
+    this.loader.present();
+    try {
+      this.getCurrentLocation('loadMap');
+    } catch (error) {
+      alert(error);
+    }
   }
-
-  getCurrentLocation(){
+  getCurrentLocation(callback){
     let options = { timeout:10000 , enableHighAccuracy:true };
-    this.location = Observable.create(observable => {
-      let prevLat = 0;
-      let prevLng = 0;
-      this.loader.present();
-        this.geolocation.getCurrentPosition(options).then((position) => {
-          let latitude = position.coords.latitude;
-          let longitude = position.coords.longitude;
-          if(prevLat !== latitude || prevLng !== longitude) {
-            observable.next({lat: latitude , lng: longitude});
-          }
-        }, (err) => {
-          console.log(err);
-        })
+      this.geolocation.getCurrentPosition(options).then((position) => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        this[callback]({lat: latitude , lng: longitude});
+      }, (err) => {
+        console.log(err);
     });
   }
 
-  loadMap(){
-    this.location.subscribe(location => {
-      this.map = new GoogleMap('map', {
-        'mapType' : 'MAP_TYPE_ROADMAP',
-        'controls': {
-          'compass': false,
-          'myLocationButton': true,
-          'indoorPicker': true,
-          'zoom': false
-        },
-        'gestures': {
-          'scroll': true,
-          'tilt': true,
-          'rotate': true,
-          'zoom': true
-        },
-        'camera': {
-          'target': location,
-          'tilt': 30,
-          'zoom': 15
-        }
-      });
-
-      this.addMarker(location);
-    });
+  loadMap(location){
+      if(!this.map) {
+        let element = document.getElementById('map');
+        this.map = new GoogleMap(element, {
+          'controls': {
+            'compass': false,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': false
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'target': location,
+            'tilt': 30,
+            'zoom': 17
+          }
+        });
+      }else {
+        this.map.setCameraTarget(location);
+      }
+      try {
+        this.addMarker(location);
+      } catch (err) {
+        alert(JSON.stringify(err));
+      }
   }
 
   addMarker(location) {
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      this.map.moveCamera(location);
+      this.getLocationText(location);
+
+      this.map.on(GoogleMapsEvent.CAMERA_MOVE_END).subscribe(() => {
+        this.doFetch();
+      });
       this.loader.dismiss();
-      /*this.map.addMarker({
-        title: 'Pickup Location',
-        icon: {
-          url : 'assets/imgs/locator.png',
-          size: {
-            width: 60,
-            height: 50
-          }
-        },
-        position: location ,
-        draggable: true
-      })
-        .then(marker => {
-          marker.on(GoogleMapsEvent.MARKER_DRAG_END)
-            .subscribe(() => {
-              //alert('clicked');
-            });
-        });*/
+      this.updatePickUpLocation();
     });
+  }
 
-    this.map.one(GoogleMapsEvent.MAP_DRAG_END).then(() => {
+  doFetch() {
+    let loc = this.map.getCameraPosition();
+    this.getLocationText(loc.target);
+    this.updatePickUpLocation();
+  }
 
+  getLocationText(loc) {
+    this.forceUpdate('pickupLocation' , 'Fetching Address........');
+    let geocoder = new google.maps.Geocoder();
+    this.geoTracker = Observable.create((observer) => {
+      geocoder.geocode({'location': loc}, function(results, status) {
+        if (status === 'OK') {
+          if (results[0]) {
+            observer.next(results[0].formatted_address);
+          }
+        } else {
+          alert('Geocoder failed due to: ' + status);
+        }
+      });
+    });
+  }
+
+  forceUpdate(field , value) {
+    this.ngZone.run(() => {
+      this[field] = value;
+    });
+  }
+
+  updatePickUpLocation() {
+    this.geoTracker.subscribe((value) => {
+      this.forceUpdate('pickupLocation' , value);
     });
   }
 }
